0af2dc24e4b34c3f2193624f7ea14233fd033b1c
Gabrielle Noel <80295813+gabriellenoel@users.noreply.github.com>
2021-03-10 13:25:08 -0500

    redo hierarchy file

--- src/hierarchy.java
+++ src/hierarchy.java
@@ -2,40 +2,70 @@
 public class hierarchy {
 
     interface Shape {
-        public void shapeName(); // interface method (does not have a body)
-        public void shapeFeatures(); // interface method (does not have a body)
+        public float calculateArea(); // interface method (does not have a body)
+        public void display(); // interface method (does not have a body)
     }
 
     public static class Rectangle implements Shape {
-        public void shapeName() {
-            System.out.println("This shape is a circle.");
+        private float length, width;
+
+        public Rectangle(float length, float width) {
+            this.length = length;
+            this.width = width;
         }
-        public void shapeFeatures() {
-            System.out.println("This shape has four sides.");
+
+        public float calculateArea() {
+            return length*width;
+        }
+        public void display() {
+            System.out.println("The rectangle length is " + length + " and the width is " + width);
+            System.out.println("The area of this rectangle is: " + calculateArea());
         }
     }
 
     public static class Circle implements Shape {
-        public void shapeName() {
-           System.out.println("This shape is a circle.");
+        private float radius;
+
+        public Circle(float radius) {
+            this.radius = radius;
         }
-        public void shapeFeatures() {
-            System.out.println("This shape is round.");
+
+        public float calculateArea() {
+            return (float) Math.PI * (radius * radius);
+        }
+        public void display() {
+            System.out.println("The radius of the circle is " + radius);
+            System.out.println("The area of circle is " + calculateArea());
         }
     }
 
     public static class Triangle implements Shape {
-        public void shapeName() {
-            System.out.println("This shape is a triangle.");
+
+        private float base, height;
+
+        public Triangle(float base, float height) {
+            this.base = base;
+            this.height = height;
         }
-        public void shapeFeatures() {
-            System.out.println("This shape has three sides.");
+
+        public float calculateArea() {
+            return (base * height)/2;
+        }
+        public void display() {
+            System.out.println("The triangle width is " + base + " and the height is " + height);
+            System.out.println("The area of the triangle is " + calculateArea());
         }
     }
 
         public static void main(String[] args) {
-            Circle firstShape = new Circle();
-            firstShape.shapeName();
-            firstShape.shapeFeatures();
+
+                Triangle triangle = new Triangle(5, 5);
+                triangle.display();
+
+                Rectangle rect = new Rectangle(5,5);
+                rect.display();
+
+                Circle circle = new Circle(5);
+                circle.display();
+            }
         }
-}
